ae81f26bdf9c7332533ed511d8f75e5885bcfd8f
kuu4357 <kuu4357@gmail.com>
2020-12-20 10:10:50 +0000

    top page

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
-Rails.applecation.config.generators do |g|
-    g.stylesheets false
-    g.javescripts false
-    g.helper false
-    g.skip_routes true
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
 end
