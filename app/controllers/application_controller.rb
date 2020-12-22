77048cf9574b1859198edcb244888f1f2043d958
kuu4357 <kuu4357@gmail.com>
2020-12-22 07:21:58 +0000

    user_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  def index
+  include SessionHelper
+  private
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
   end
 end
